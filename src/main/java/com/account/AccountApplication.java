d24c402fd656e8734ce3ac39a617699e468920cd
krittamet <krittamateza@gmail.com>
2026-03-23 02:16:56 +0700

    Remove unnecessary KafkaAutoConfiguration import from AccountApplication and ensure bean definition overriding is enabled in application.properties

--- src/main/java/com/account/AccountApplication.java
+++ src/main/java/com/account/AccountApplication.java
@@ -4,13 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
-import org.springframework.context.annotation.Import;
 
 @EnableKafka
 @EnableDiscoveryClient
 @SpringBootApplication
-@Import(KafkaAutoConfiguration.class)
 public class AccountApplication {
     public static void main(String[] args) {
         SpringApplication.run(AccountApplication.class, args);
